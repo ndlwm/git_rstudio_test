9a9f04f7b4ec792c1a268ebc5b8288adc69b237b
ndlwm <liweiming@gmail.com>
2020-07-11 02:09:48 -0500

    Second round to test commit, push rstudio_test.R to GitHub

--- rstudio_test.R
+++ rstudio_test.R
@@ -3,3 +3,5 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 print("Now the 3rd print line is added by direct GitHub master edit")
+
+print("This is the second round test to add a line and commit, push to GitHub")
